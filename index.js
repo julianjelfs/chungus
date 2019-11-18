60dcafecb4b2aeb2939b26e7652c38ce454ceeac
Julian Jelfs <julian.jelfs@ef.com>
2019-11-18 17:30:17 +0000

    added assets for the tutorial

---
+++ index.js
@@ -0,0 +1,40 @@
+import Phaser  from "phaser";
+import sky from "./assets/sky.png";
+import ground from "./assets/platform.png";
+import star from "./assets/star.png";
+import bomb from "./assets/bomb.png";
+import dude from "./assets/dude.png";
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: "100%",
+    height: "100%",
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('sky', sky);
+    this.load.image('ground', ground);
+    this.load.image('star', star);
+    this.load.image('bomb', bomb);
+    this.load.spritesheet('dude', 
+        dude,
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+function create () {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
+}
+
+function update ()
+{
+}
